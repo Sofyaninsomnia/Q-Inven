9778d9a7c5759c3d3b3ef2a7146a44146ed9ea59
Sofyaninsomnia <ngodingsekarepe@gmail.com>
2026-02-19 13:13:47 +0700

    layouting & routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Dashboard;
+use App\Http\Controllers\Petugas\Dashboard as PetugasDashboard;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->name('admin.')->namespace('App\Http\Controllers\Admin')->group(function () {
+    Route::get('/', [Dashboard::class, 'index'])->name('index');
 });
+
+Route::get('/petugas', [PetugasDashboard::class, 'index'])->name('petugas');
